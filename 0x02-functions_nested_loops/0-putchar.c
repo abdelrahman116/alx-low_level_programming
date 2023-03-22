32f1782836a0b029a591ac7e928374ee1f9f57b4
Abdoo <abdelrahman@github.com>
2023-03-21 17:56:54 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main()
+{
+	printf("_putchar\n");
+	/* your code goes there */
+	return (0);
+}
