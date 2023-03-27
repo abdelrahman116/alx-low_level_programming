8e09b0c10b94440ff75c8ada595fe54e419b8d3c
Abdoo <abdelrahman@github.com>
2023-03-26 23:55:08 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
-/* function that takes a pointer to an int as parameter and updates the value it points to to 98.*/
+/**
+ * function that takes a pointer to an int as parameter 
+ * main - check the code 
+ *and updates the value it points to to 98.
+ */
 int main(void)
-{int n = 100;
+{
+int n = 100;
 int *p = &n;
 *p = 98;
 }
