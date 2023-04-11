cf60b7836387d662abff6940bb27769b31e820b5
Abdoo <abdelrahman@github.com>
2023-04-11 15:30:47 -0700

    DONE

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - introduction to malloc and free
+ *
+ * Return: 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i ;
+	i=0;
+	if (size <= 0)
+	{
+		return NULL ;
+	} 
+
+
+    return (0);
+}
