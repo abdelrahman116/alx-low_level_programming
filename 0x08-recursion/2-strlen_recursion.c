338a576df47c2c41b8427a937e6229160feac98a
Abdoo <abdelrahman@github.com>
2023-04-24 01:15:49 -0700

    strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,14 +4,14 @@
 #include <ctype.h>
 int chars (char c)
 {
-if (tolower(c) >= 'A' || tolower(c) <= 'z')
-return 1;
-else
-return 0 ;
+    if (tolower(c) >= 'A' || tolower(c) <= 'z')
+        return 1;
+    else
+        return 0 ;
 }
 int _strlen_recursion(char *s)
 {
-if (strlen(s) == 0)
-return 0;
-return chars(s[0]) + _strlen_recursion(s+1);
+    if (strlen(s) == 0)
+        return 0;
+    return chars(s[0]) + _strlen_recursion(s+1);
 }
