d6912810ff5aa614554a1c6d4758956b9e29ebe5
abdelrahman <abdelrahman@github.com>
2023-05-04 13:38:33 -0700

    binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,21 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <"main.h"
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- *
- * Return: the converted number
- */
+*binary_to_uint-converts from binary to dicimal
+*@b: The string of numbers
+*Return: the conversion result
+*/
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+        unsigned int x, result = 0;
 
-	if (!b)
-		return (0);
-
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
-
-	return (dec_val);
+        if (!b)
+                return (0);
+        for (x = 0 ; b[x] ; x++)
+        {
+        if (b[x] > '1' || b[x] < '0')
+        return (0);
+        result = 2 * result + (b[x] - '0');
+        }
+        return (result);
 }
-
