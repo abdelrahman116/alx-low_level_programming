6f02b826bd876448d9fad1d81fa35d0b97309fac
abdelrahman <abdelrahman@github.com>
2023-05-05 14:06:09 -0700

    binary to unit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,10 +4,14 @@
 *@bits : the specific bit
 *@index : the index
 *@n : the number
-* Return : the bit
+* Return: the bit
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
+if (index > 63)
+{
+	return (-1);
+}
 int bits;
 
 bits = (n >> index) & 1;
