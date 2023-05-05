ae56a261448f240027cc0ac1853d3445a2f8651e
abdelrahman <abdelrahman@github.com>
2023-05-05 13:29:31 -0700

    binary to unit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,16 +1,24 @@
 #include "main.h"
 /**
-*print_binary-converts from binary to dicimal
-*@n: The string of numbers
-*putchar: the conversion result
-*/
+ * print_binary - prints the binary representation of a number
+ * @n: the number to be printed in binary
+ *
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
-	unsigned long int i;
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
 
-	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8  - 1);
-
-	for (i = 0; i < sizeof(unsigned long int) * 8 ; i++)
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+	while (mask > 0 && !(n & mask))
+	{
+	mask >>= 1;
+	}
+	while (mask > 0)
 	{
 		putchar((n & mask) ? '1' : '0');
 		mask >>= 1;
