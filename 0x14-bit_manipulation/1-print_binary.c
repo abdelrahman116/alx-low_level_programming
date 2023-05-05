bb211fef57cf7e1eef1ed711ccb3261ff970837c
abdelrahman <abdelrahman@github.com>
2023-05-05 12:46:31 -0700

    binary to unit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,9 +7,12 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int i;
+
 	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8  - 1);
-	for (i = 0; i < sizeof(unsigned long int) * 8 ; i++) {
-		putchar((n & mask) ? '1' : '0'); 
-		mask >>= 1; 
-    }
+
+	for (i = 0; i < sizeof(unsigned long int) * 8 ; i++)
+	{
+		putchar((n & mask) ? '1' : '0');
+		mask >>= 1;
+	}
 }
