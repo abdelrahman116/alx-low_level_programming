de276085c1853e8d99ad019c1596e46e38b609d6
abdelrahman <abdelrahman@github.com>
2023-06-13 14:58:10 -0700

    0-create_array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+/**
+ * create_array - creates an array
+ *@size: The size of the array
+ *@c: The charactars of the array
+ *
+ * Return: A.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+char *create_array(unsigned int size, char c)
+{
+	char *A;
+	int i;
+
+	if (size <= 0)
+	return (NULL);
+	A = (char *)malloc(size * sizeof(char));
+	for (i = 0 ;(unsigned) i < size ; i++)
+	A[i] = c;
+	return (A);
+}
