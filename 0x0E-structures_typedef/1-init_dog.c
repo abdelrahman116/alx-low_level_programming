ea39e2e6ba004471d6adf292d4689ece59eb12c2
abdelrahman <abdelrahman@github.com>
2023-06-19 18:11:53 -0700

    my_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+#include "dog.h"
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog *d ;
+dog.name = name;
+dog.age = age;
+dog.owner = owner;
+}
+
